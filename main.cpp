8b4ea84d676eb1cd318b7735383887221c7665de
kingarmin <arminghajari1384@gmail.com@gmail.com>
2024-11-15 18:18:02 +0330

    Q3

---
+++ main.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    char simple_intersection=197,simple_row=196,row=205 , left_down_corner=200,left_up_corner=201,right_down_corner=188,right_up_corner=187,column=186,intersection_left=204,intersection_right=185,intersection_up=202,intersection_down=203,column2=179;
+    string names[]={"Armin Ghajari","Aria Mohammadi","Mohammad Naseri","Nika fallahi","Shahin abasi","Romina Sepahyan","abas najafi","Arvin Khosravi","ali mohammadi","Reza Soltani"};
+
+    cout<<left_up_corner;
+    for (int i=0 ; i<3 ; i++){
+        cout<<setfill(row)<<setw(24)<<intersection_down;
+    }
+    cout<<setfill(row)<<setw(24)<<right_up_corner<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(13)<<setfill(' ')<<"row"<<setw(11)<<setfill(' ')<<column2<<setw(13)<<setfill(' ')<<"id"<<setw(11)<<setfill(' ')<<column2<<setw(17)<<setfill(' ')<<"fisrt_name"<<setw(7)<<setfill(' ')<<column2<<setw(16)<<setfill(' ')<<"last_name"<<setw(8)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'1'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"1001"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Armin"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Ghajari"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'2'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"2002"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Aria"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Mohammadi"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'3'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"3003"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Nika"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Falahi"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'4'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"4004"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Romina"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Sepahi"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'5'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"5005"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Arian"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Abasi"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'6'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"6006"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Amir"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Amiri"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'7'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"7007"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Kian"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Sharifan"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'8'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"8008"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Eminem"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Jamshidi"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<'9'<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"9009"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Yekta"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Parsa"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<intersection_left<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(simple_row)<<simple_intersection;
+    }
+
+
+
+    cout<<setw(24)<<setfill(simple_row)<<intersection_right<<endl<<column;
+
+
+
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<column<<endl<<column<<setw(12)<<setfill(' ')<<"10"<<setw(12)<<setfill(' ')<<column2<<setw(14)<<setfill(' ')<<"1010"<<setw(10)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Sana"<<setw(12)<<setfill(' ')<<column2<<setw(12)<<setfill(' ')<<"Ziaie"<<setw(12)<<setfill(' ')<<column<<endl<<column;
+    for(int i=0 ; i<3 ; i++){
+        cout<<setw(24)<<setfill(' ')<<column2;
+    }
+    cout<<setw(24)<<setfill(' ')<<column<<endl<<left_down_corner<<setw(24)<<setfill(row)<<intersection_up;
+    for(int i=0 ; i<2 ; i++){
+        cout<<setw(24)<<setfill(row)<<intersection_up;
+    }
+    cout<<setw(24)<<setfill(row)<<right_down_corner;
+    return 0;
+}
